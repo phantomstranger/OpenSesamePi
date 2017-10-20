ee11ce6a4c3a5997b14da9286892900e73a9d089
phantomstranger <mccalleyt@gmail.com>
2017-10-20 11:54:51 -0500

    Total Rework to add the ability for expansion of the program.

--- DoorChecker.py
+++ DoorChecker.py
@@ -1,78 +1,54 @@
 #!/usr/bin/env python
-
-"""
-DoorChecker.py
-
-This is primary reed switch checking logic, designed to detect when the
-switch (door) is opened or closed, and to react accordingly.
-
-The GPIO pin corresponding to the switch attached to the door, which is
-defined in the corresponding settings.py file.
-
-"""
-
-from settings import DOOR_PIN, SLEEP_TIME, NOTIFY_LIST
-
+from gpiozero import Button
+from signal import pause
 import time
 from datetime import datetime
 import subprocess
 from string import Template
 
-#
-# Notification by email
+SLEEP_TIME  = 0.01
+
+NOTIFY_LIST = []
+with open('email.conf','r') as notify:
+  for word in notify.read().split():
+    NOTIFY_LIST.append(word)
+
+ACTPINS = {}
+with open('pipins.conf','r') as my_file:
+   for line in my_file:
+     l_split = line.split()
+     ACTPINS[int(l_split[0])] = l_split[1]
 
 def current_date (fmt="%a %d-%m-%Y @ %H:%M:%S"):
-    """Return the current time and date as a string, 
-       for the email notification message.
-
-       The default format returns a string like this:
-       'Sun 01-11-2015 @ 14:50:50'
-    """       
-
     return datetime.strftime(datetime.now(), fmt)
 
-NOTIFY_CMD = Template("""echo "$date door $state" | mail -s "Pi: door $state" $email""")
-
-def notify (door_state):
+NOTIFY_CMD = {pin: Template("""echo "$date $sensor $state" | mail -s "Pi: $sensor $state" $email""") for pin in ACTPINS}
+ 
+def notify (id, state, sensor_name):
     """Send each of the email addresses in NOTIFY_LIST a message"""
-
     for email in NOTIFY_LIST:
-        shell_cmd = NOTIFY_CMD.substitute(date=current_date(),
-                                          state=door_state,
-                                          email=email)
-        proc = subprocess.Popen(shell_cmd,
-                                shell=True,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
+        shell_cmd = NOTIFY_CMD[id].substitute(date=current_date(),
+                                  state=state, sensor=sensor_name, email=email)
+        proc = subprocess.Popen(shell_cmd, shell=True,
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout_value, stderr_value = proc.communicate()
+        with open('log.txt', 'a') as f:
+          f.write('{}\n'.format(shell_cmd))
+ 
+def do_action(button):
+  # function to send email etc will only run when opened or closed
+  state = 'closed' if button.is_pressed else 'opened'
+  notify(button.pin, state, ACTPINS[button.pin])
+  print(state)
 
+buttons = {}
+for id in ACTPINS:
+  buttons[id] = Button(id)
+  buttons[id].when_pressed = do_action
+  buttons[id].when_released = do_action
+  print(id)
+  print(buttons)
+  print(ACTPINS)
+  print(NOTIFY_LIST)
 
-#
-# GPIO logic
-
-import RPi.GPIO as io
-io.setmode(io.BCM)
-
-io.setup(DOOR_PIN, io.IN,
-         pull_up_down=io.PUD_UP) # activate the reed input with PullUp
-
-STATES = {
-    'current': 'closed',
-    'prior'  : 'closed'
-}
-
-while True:
-    if io.input(DOOR_PIN):
-        # door is closed
-        STATES['current'] = 'closed'
-        if STATES['prior'] == 'opened':
-            STATES['prior'] = 'closed'
-            notify('closed')
-    else:
-        # door is open
-        STATES['current'] = 'opened'
-        if STATES['prior'] == 'closed':
-            STATES['prior'] = 'opened'
-            notify('opened')
-        
-    time.sleep(SLEEP_TIME)
+pause()
